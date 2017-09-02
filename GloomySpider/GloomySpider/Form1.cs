9b838316099864cfa002f3d84b516da196dbcf7a
JoonKR <sjy8547@gmail.com>
2017-09-03 08:50:48 +0900

    핵

--- GloomySpider/GloomySpider/Form1.cs
+++ GloomySpider/GloomySpider/Form1.cs
@@ -15,8 +15,8 @@ namespace GloomySpider
 {
     public partial class MainFrom : MetroFramework.Forms.MetroForm
     {
-        long LogInResult;
-        int conditionLoadState;
+        private long LogInResult;
+        private int conditionLoadState;
         private int _scrNum = 5000;
         public MainFrom()
         {
@@ -31,6 +31,7 @@ namespace GloomySpider
             axKHOpenAPI.OnReceiveConditionVer += API_OnReceiveConditionVer;
         }
 
+        #region 키움 API 이벤트
         private void API_OnReceiveConditionVer(object sender, _DKHOpenAPIEvents_OnReceiveConditionVerEvent e)
         {
             this.listviewConditionSearchList.Clear();
@@ -57,7 +58,7 @@ namespace GloomySpider
 
         private void API_OnReceiveRealData(object sender, _DKHOpenAPIEvents_OnReceiveRealDataEvent e)
         {
-    
+
         }
 
         private void API_OnReceiveRealCondition(object sender, _DKHOpenAPIEvents_OnReceiveRealConditionEvent e)
@@ -68,16 +69,16 @@ namespace GloomySpider
             //BSTR strConditionIndex    // 조건명 인덱스
 
             string type = e.strType.Equals("I") ? "종목편입" : "종목이탈";
-            Logger(Log.일반,e.strConditionName+";"+ type + ";" + e.sTrCode);
+            Logger(Log.일반, e.strConditionName + ";" + type + ";" + e.sTrCode);
         }
 
         private void API_OnReceiveTrData(object sender, _DKHOpenAPIEvents_OnReceiveTrDataEvent e)
         {
-            if(e.sRQName.Equals("조건검색결과"))
+            if (e.sRQName.Equals("조건검색결과"))
             {
                 int count = axKHOpenAPI.GetRepeatCnt(e.sTrCode, e.sRQName);
 
-                for(int i = 0; i < count;i++)
+                for (int i = 0; i < count; i++)
                 {
                     string stockName = axKHOpenAPI.GetCommData(e.sTrCode, e.sRQName, i, "종목명").Trim();
                     string stockCurrentPrice = axKHOpenAPI.GetCommData(e.sTrCode, e.sRQName, i, "현재가").Trim();
@@ -116,7 +117,9 @@ namespace GloomySpider
             }
             UpdateUserInformation();
         }
+        #endregion
 
+        #region UI 이벤트
         private void bttnStart_Click(object sender, EventArgs e)
         {
             if (axKHOpenAPI.GetConnectState() == 0)
@@ -128,32 +131,6 @@ namespace GloomySpider
                 Logger(Log.일반, "연결중");
             }
         }
-
-     
-
-        private void UpdateUserInformation()
-        {
-            richTextBox1.Text = "";
-            richTextBox1.Text += "LOGIN ID : " + axKHOpenAPI.GetLoginInfo("USER_ID");
-            string account = axKHOpenAPI.GetLoginInfo("ACCNO");
-            string[] accounts = account.Split(';');
-            richTextBox1.Text += "\n 보유한 계좌 : " + axKHOpenAPI.GetLoginInfo("ACCOUNT_CNT");
-
-            foreach (string acc in accounts)
-            {
-                if (string.IsNullOrEmpty(acc))
-                    continue;
-
-                richTextBox1.Text += "\n Account : " + acc;
-                if (string.IsNullOrEmpty(tbAccount.Text))
-                {
-                    tbAccount.Text = acc;
-                }
-            }
-
-            richTextBox1.Text += "\n 사용자명 : " + axKHOpenAPI.GetLoginInfo("USER_NAME");
-        }
-
 
         private void metroButton1_Click(object sender, EventArgs e)
         {
@@ -171,30 +148,7 @@ namespace GloomySpider
             }
         }
 
-        private string GetScreenNum()
-        {
-            if (_scrNum < 9000)
-                this._scrNum++;
-            else
-                this._scrNum = 5000;
-            return this._scrNum.ToString();
-        }
 
-        private void btnBuy_Click(object sender, EventArgs e)
-        {
-            string orderGb = "00";
-            int orderPrice = 0;
-            if (metroRadioButton2.Checked)
-            {
-                orderGb = "03";
-            }
-            else
-            {
-                orderGb = "00";
-                orderPrice = int.Parse(tbOrderPrice.Text);
-            }
-            sendOrderGS(1, tbStockCode.Text, int.Parse(tbOrderQty.Text), orderPrice, orderGb);
-        }
 
         private void btnSell_Click(object sender, EventArgs e)
         {
@@ -236,16 +190,63 @@ namespace GloomySpider
             }
         }
 
+        private void bttnConditionSearchStart_Click(object sender, EventArgs e)
+        {
+            if (this.listviewConditionSearchList.SelectedItems.Count == 0)
+                return;
+
+
+            foreach (ListViewItem lvItem in this.listviewConditionSearchList.Items)
+            {
+                string[] conditionInfo1 = lvItem.Text.Split(';');
+                int conditionIndex1 = Int32.Parse(conditionInfo1[0]);
+                string conditionName1 = conditionInfo1[1];
+
+                axKHOpenAPI.SendConditionStop(GetScreenNum(), conditionName1, conditionIndex1);
+            }
+
+            string[] conditionInfo = this.listviewConditionSearchList.SelectedItems[0].Text.Split(';');
+            int conditionIndex = Int32.Parse(conditionInfo[0]);
+            string conditionName = conditionInfo[1];
+            int result = axKHOpenAPI.SendCondition(GetScreenNum(), conditionName, conditionIndex, 1);
+
+            if (result > 0)
+                Logger(Log.일반, "조건 검색 결과 불러오기 완료");
+            else
+                Logger(Log.에러, "조건 검색 결과 불러오기 실패");
+        }
+
+
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            string orderGb = "00";
+            int orderPrice = 0;
+            if (metroRadioButton2.Checked)
+            {
+                orderGb = "03";
+            }
+            else
+            {
+                orderGb = "00";
+                orderPrice = int.Parse(tbOrderPrice.Text);
+            }
+            sendOrderGS(1, tbStockCode.Text, int.Parse(tbOrderQty.Text), orderPrice, orderGb);
+        }
+        #endregion
+
+        #region 사용자 함수
+
         /*  Parameter 설명
-            거래유형 orderType :  1:신규매수, 2:신규매도 3:매수취소, 4:매도취소, 5:매수정정, 6:매도정정
-            종목코드 stockCode
-            주문수량 orderQty
-            주문가격 orderPrice
-            거래구분 orderGb
-              - 00 : 지정가
-              - 03 : 시장가
-            원주문번호 orgOrderNo : 일반적으론 주문정정이나 취소가 아닐때는 안넣어도 됨
-        */
+           거래유형 orderType :  1:신규매수, 2:신규매도 3:매수취소, 4:매도취소, 5:매수정정, 6:매도정정
+           종목코드 stockCode
+           주문수량 orderQty
+           주문가격 orderPrice
+           거래구분 orderGb
+             - 00 : 지정가
+             - 03 : 시장가
+           원주문번호 orgOrderNo : 일반적으론 주문정정이나 취소가 아닐때는 안넣어도 됨
+       */
 
         private void sendOrderGS(int orderType, string stockCode, int orderQty, int orderPrice, string orderGb, string orgOrderNo = "")
         {
@@ -296,40 +297,43 @@ namespace GloomySpider
             }
         }
 
-
-
-        
-
-        private void bttnConditionSearchStart_Click(object sender, EventArgs e)
+        private string GetScreenNum()
         {
-            if (this.listviewConditionSearchList.SelectedItems.Count == 0)
-                return;
+            if (_scrNum < 9000)
+                this._scrNum++;
+            else
+                this._scrNum = 5000;
+            return this._scrNum.ToString();
+        }
 
+        private void UpdateUserInformation()
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Text += "LOGIN ID : " + axKHOpenAPI.GetLoginInfo("USER_ID");
+            string account = axKHOpenAPI.GetLoginInfo("ACCNO");
+            string[] accounts = account.Split(';');
+            richTextBox1.Text += "\n 보유한 계좌 : " + axKHOpenAPI.GetLoginInfo("ACCOUNT_CNT");
 
-            foreach (ListViewItem lvItem in this.listviewConditionSearchList.Items)
+            foreach (string acc in accounts)
             {
-                string[] conditionInfo1 = lvItem.Text.Split(';');
-                int conditionIndex1 = Int32.Parse(conditionInfo1[0]);
-                string conditionName1 = conditionInfo1[1];
+                if (string.IsNullOrEmpty(acc))
+                    continue;
 
-                axKHOpenAPI.SendConditionStop(GetScreenNum(), conditionName1, conditionIndex1);
+                richTextBox1.Text += "\n Account : " + acc;
+                if (string.IsNullOrEmpty(tbAccount.Text))
+                {
+                    tbAccount.Text = acc;
+                }
             }
 
-            string[] conditionInfo = this.listviewConditionSearchList.SelectedItems[0].Text.Split(';');
-            int conditionIndex = Int32.Parse(conditionInfo[0]);
-            string conditionName = conditionInfo[1];
-            int result = axKHOpenAPI.SendCondition(GetScreenNum(), conditionName, conditionIndex, 1);
-
-            if (result > 0)
-                Logger(Log.일반, "조건 검색 결과 불러오기 완료");
-            else
-                Logger(Log.에러, "조건 검색 결과 불러오기 실패");
+            richTextBox1.Text += "\n 사용자명 : " + axKHOpenAPI.GetLoginInfo("USER_NAME");
         }
 
         // 로그를 출력합니다.
         public void Logger(Log type, string msg)
         {
             listviewLog.Items.Add(type.ToString() + " : " + msg);
-        }
+        } 
+        #endregion
     }
 }
